3edf0b211303ee8083a89a0d1eb1a8d5e8b899ec
Dash <darsh.raheja4@gmail.com>
2020-07-31 20:02:13 +0530

    i made a slingshot

--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
 var bird, slingShot;
+var sli;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -38,8 +39,7 @@ function setup(){
 
     bird = new Bird(100,100);
 
-    log6 = new Log(230,180,80, PI/2);
-    chain = new Chain(bird.body,log6.body);
+    sli = new Slingshot(bird.body,{x:200 , y:200});
 }
 
 function draw(){
@@ -63,6 +63,13 @@ function draw(){
 
     bird.display();
     platform.display();
-    log6.display();
-    chain.display();    
+    sli.display();    
+}
+
+
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY})
+}
+function mouseReleased(){
+    sli.fly();
 }
